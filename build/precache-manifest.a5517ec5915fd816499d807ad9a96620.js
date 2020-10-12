1aa14348047bf48092b347bb279889c43f9942b4
RYOTA <kobaryo04ysh@icloud.com>
2020-10-12 23:03:13 +0900

    first commit

---
+++ build/precache-manifest.a5517ec5915fd816499d807ad9a96620.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f77c0b10f3fda644452bdd813df27860",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0da839769441165bb20c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0899288ff7a894c152cb",
+    "url": "/static/js/2.fdd95fb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fdd95fb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0da839769441165bb20c",
+    "url": "/static/js/main.c88a6b93.chunk.js"
+  },
+  {
+    "revision": "ae9906e79bde8488aac1",
+    "url": "/static/js/runtime-main.04d548d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
